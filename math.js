0991d48926063a703a8fca55a8a03696c707a7bb
ECastro08 <worldmechatronics2023@gmail.com>
2023-11-08 13:12:11 -0500

    commit

--- math.js
+++ math.js
@@ -1,2 +1,15 @@
-const ladoCuadrado = 5;
-const perimetroCuadrado = lado * 4;
+const lado = 5;
+const perimetro = lado * 4;
+const area = lado ** 2;
+
+console.log({
+    lado,
+    perimetro,
+    area,
+});
+
+const baseTriangulo = 3;
+const alturaTriangulo = 4;
+
+const perimetroTriangulo = baseTriangulo + alturaTriangulo ;
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
